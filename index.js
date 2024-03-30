8249ba07ede7350177749aed5aa2dbe45bdd0e36
Durgesh kumar <147022469+Durgesh1236@users.noreply.github.com>
2024-03-30 11:26:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// Function to play the video when clicked
+function playVideo(videoId) {
+    var video = document.getElementById(videoId);
+    video.play();
+}
+
+// Function to pause the video when clicked
+function pauseVideo(videoId) {
+    var video = document.getElementById(videoId);
+    video.pause();
+}
